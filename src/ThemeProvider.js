311512ee1afbca058d3cab2931868a308b9e13dd
Khanh Pham <vankhanhpf96@gmail.com>
2019-10-28 16:22:33 +0700

    Replace JSX by createElement

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -1,12 +1,8 @@
-import React, {createContext} from 'react'
+import {createContext, createElement} from 'react'
 
 const ThemeContext = createContext()
 
-const ThemeProvider = ({theme, children}) => (
-  <ThemeContext.Provider value={theme}>
-    {children}
-  </ThemeContext.Provider>
-)
+const ThemeProvider = ({theme, children}) => createElement(ThemeContext.Provider, {value: theme}, children)
 
 export {ThemeContext}
 
